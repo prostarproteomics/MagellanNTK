3ef415ad7a5f5e8faa1e8119460e69b73679eae5
Samuel Wieczorek <samuel.wieczorek@cea.fr>
2024-07-01 05:59:31 +0200

    Update

--- R/mod_mainapp.R
+++ R/mod_mainapp.R
@@ -131,28 +131,28 @@ mainapp_ui <- function(id, session){
           
         ),
         sidebar = uiOutput(ns('sidebar')),
-        controlbar = shinydashboardPlus::dashboardControlbar(
-          skin = "dark",
-          shinydashboardPlus::controlbarMenu(
-            shinydashboardPlus::controlbarItem(
-              title = "Configure",
-              icon = icon("desktop"),
-              active = TRUE,
-              actionLink(ns('browser'), 'Console'),
-              mod_modalDialog_ui(ns('loadPkg_modal'))
-            ),
-            shinydashboardPlus::controlbarItem(
-              icon = icon("paint-brush"),
-              title = "Settings",
-              mod_settings_ui(ns('global_settings'))
-            )
-            ,shinydashboardPlus::controlbarItem(
-              icon = icon("paint-brush"),
-              title = "Skin",
-              shinydashboardPlus::skinSelector()
-            )
-          )
-          ),
+        # controlbar = shinydashboardPlus::dashboardControlbar(
+        #   skin = "dark",
+        #   shinydashboardPlus::controlbarMenu(
+        #     shinydashboardPlus::controlbarItem(
+        #       title = "Configure",
+        #       icon = icon("desktop"),
+        #       active = TRUE,
+        #       actionLink(ns('browser'), 'Console'),
+        #       mod_modalDialog_ui(ns('loadPkg_modal'))
+        #     ),
+        #     shinydashboardPlus::controlbarItem(
+        #       icon = icon("paint-brush"),
+        #       title = "Settings",
+        #       mod_settings_ui(ns('global_settings'))
+        #     )
+        #     ,shinydashboardPlus::controlbarItem(
+        #       icon = icon("paint-brush"),
+        #       title = "Skin",
+        #       shinydashboardPlus::skinSelector()
+        #     )
+        #   )
+        #   ),
         body = shinydashboard::dashboardBody(
           # some styling
           tags$head(
