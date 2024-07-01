7d30e88c23029b528422816818c65cffeed8d3f8
Samuel Wieczorek <samuel.wieczorek@cea.fr>
2024-07-01 10:41:56 +0200

    Update

--- R/MagellanNTK.R
+++ R/MagellanNTK.R
@@ -164,10 +164,7 @@ MagellanNTK <- function(
   # Launch app
   app <- shiny::shinyApp(ui, server)
   
-  if (usermod == "dev"){
-    shiny::runApp(app)
-  } else if (usermod == 'user') {
-    shiny::runApp(app, host = "127.0.0.1", port = 3838, launch.browser=TRUE)
-  }
+  
+    shiny::runApp(app,launch.browser=usermod == 'user')
 
 }
